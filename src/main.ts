f12d5310b166299688aabe8df83cbea94c38fd53
newxxson <hyukjun1111@gmail.com>
2024-02-12 19:48:57 +0900

    [FEAT] <혁준> made nest server to auto-reload upon modification of code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws';
+import { SocketIoAdapter } from './adapters/socket-io.adapters';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'], // More verbose logging
   });
+  app.useWebSocketAdapter(new SocketIoAdapter(app));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   await app.listen(3000);
 }
 bootstrap();
