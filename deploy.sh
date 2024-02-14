6341a7aae744aad1e96e268908094ab5143850fa
newxxson <hyukjun1111@gmail.com>
2024-02-14 19:20:38 +0900

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
-# #!/bin/bash
-# # Custom deployment script
-# echo "Running npm install..."
-# npm install --production
-# echo "Finished npm install."
+#!/bin/bash
+# Custom Kudu deployment script
+
+# Navigate to the wwwroot folder
+cd /home/site/wwwroot
+
+# Install npm packages
+echo "Installing npm packages..."
+npm install
+
+# Build the app if you have a build step
+echo "Building the app..."
+npm run build
