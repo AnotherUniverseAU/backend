226ce238057bd393ff9cc75e772309945eb3d05c
newxxson <hyukjun1111@gmail.com>
2024-02-14 14:31:36 +0900

    changed git action yml to download dependency at the server

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  // await app.listen(process.env.PORT || 3000);
   await app.listen(3000);
 }
 bootstrap();
