359cf25ae999c601c792b10e6f9a1409ca1dfa16
newxxson <hyukjun1111@gmail.com>
2024-04-03 23:12:52 +0900

    feature 프론트에서 oauth 가능하도록 구현

--- src/oauth/oauth-service/ioauth.interface.ts
+++ src/oauth/oauth-service/ioauth.interface.ts
@@ -3,4 +3,5 @@ import { OauthDTO } from '../dto/oauth.dto';
 export interface IOauth {
   getAccessToken(code: string): Promise<string>;
   getUserInfo(code: string): Promise<OauthDTO>;
+  getUserInfoWithAccessToken(token: string): Promise<OauthDTO>;
 }
