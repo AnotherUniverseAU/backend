65ee43db977d8125cdb24a1fee039aad8bc3d17f
newxxson <hyukjun1111@gmail.com>
2024-04-03 23:14:20 +0900

    앱 푸쉬 관련 구현

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { OnEvent } from '@nestjs/event-emitter';
+import { response } from 'express';
 import * as admin from 'firebase-admin';
 import { getMessaging } from 'firebase-admin/messaging';
 import { CharacterChat } from 'src/schemas/chat-schema/character-chat.schema';
@@ -13,20 +14,40 @@ export class FirebaseService {
       'GOOGLE_APPLICATION_CREDENTIALS',
     );
     const serviceAccount = JSON.parse(serviceAccountPath);
-
-    this.firebaseApp = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
+    if (!admin.apps.length) {
+      this.firebaseApp = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    }
     console.log('Firebase app initialized');
     this.messagingService = getMessaging(this.firebaseApp);
   }
 
   //this listens to event from chatroom service
-  @OnEvent('broadcast')
+  // @OnEvent('broadcast')
   async handleBroadcastEvent(payload: CharacterChat) {
     const { _id, characterId, content } = payload;
     console.log('broadcasting to: ', characterId);
     await this.sendNotifications(payload);
+  }
+
+  async sendUserNotification(title: string, body: string[], token: string) {
+    const message = {
+      notification: {
+        title,
+        body,
+      },
+      token,
+    };
+
+    await this.messagingService
+      .send(message)
+      .then((response: any) => {
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error: any) => {
+        console.log('Error sending message:', error);
+      });
   }
 
   async sendNotifications(payload: CharacterChat) {
